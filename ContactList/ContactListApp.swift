35b77dbbe23860994a8d83216f14f71283ba1833
Varya Utkina <varya2412@mail.ru>
2024-03-16 12:52:29 +0300

    Initial Commit

---
+++ ContactList/ContactListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ContactListApp.swift
+//  ContactList
+//
+//  Created by Варвара Уткина on 16.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct ContactListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
